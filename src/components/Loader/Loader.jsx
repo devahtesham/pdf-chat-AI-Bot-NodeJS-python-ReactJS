c1b90b415ff10982edd7a190d8f7eb966000dc1f
Ahtesham Akram <ahte02254@gmail.com>
2023-08-25 10:25:45 +0500

    adding space

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import "./Loader.css"
 const Loader = () => {
   return (
     <div className='loader-wrapper'>
-        <span className="loader"></span> 
+        <span className="loader"></span>  
 
     </div>
   )
